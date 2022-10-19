158a359ee40c28a4da797865255d46062e502731
Stan <demonoid.wolf@gmail.com>
2022-10-19 14:34:37 +0200

    Update pgsql_backup_check.sh

--- pgsql_backup_check.sh
+++ pgsql_backup_check.sh
@@ -3,17 +3,14 @@
 #the owner:group of this file must be postgres 
 #set -x
 
-latest_backup_date=$(/usr/bin/sed -n '1p' /tmp/backup.txt)
-current_date=$(/usr/bin/date +%F)
+latest_backup_date=$(ls -td -- /var/lib/pgsql/backup/* | head -n 1);
+current=$(date +%s);
+last_modified=$(stat -c "%Y" $latest_backup_date);
 
-#[ -z "$latest_backup_date" ] && exit 2
-#[ -z "$current_date" ] && exit 2
-
-if [ "$current_date" == "$latest_backup_date" ]
+if [ $((current - last_modified)) -lt 86400 ];
 then
     echo "OK - psqldumps are up to date, latest backup has been created today"
     exit 0
-
 elif
     pgrep -F /tmp/backup.pid &>/dev/null
 
