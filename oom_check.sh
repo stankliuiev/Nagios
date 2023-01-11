e49bb3e3f201b6bcd7013e8e7f4ad90ac8d48ae7
Stan <demonoid.wolf@gmail.com>
2023-01-11 10:11:39 +0100

    Create oom_check.sh

---
+++ oom_check.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#set -x
+#oom invoke check. This simple script checks if oom-killer was invoked.
+#It could be useful when memory monitoring does not respond quickly enough to memory spikes
+RESULT=0
+
+# exit statuses recognized by Nagios
+OK=0
+WARNING=1
+CRITICAL=2
+UNKNOWN=3
+
+OOM_ACTION_DATE=$(grep oom-kill /var/log/messages | tail -1 | awk '{print $1,$2,$3}')
+OOM_ACTION_DATE_EPOCH=$(date -d "$OOM_ACTION_DATE" +"%s")
+ACTUAL_DATE=$(date | awk '{print $2,$3,$4}')
+ACTUAL_DATE_EPOCH=$(date -d "$ACTUAL_DATE" +"%s")
+DATE_DELAY_CRIT=$(($ACTUAL_DATE_EPOCH - 40000))
+DATE_DELAY_WARN=$(($ACTUAL_DATE_EPOCH - 80000))
+
+if [ "$OOM_ACTION_DATE_EPOCH" -gt "$DATE_DELAY_CRIT" ]
+then
+    echo "CRITICAL! Some process on the server was killed, oom-killer was invoked, call forensics"
+    EXIT=2
+elif [ "$OOM_ACTION_DATE_EPOCH" -gt "$DATE_DELAY_WARN" ]
+then
+    echo "WARNING! oom-killer was envoked recently"
+    EXIT=1
+else
+    echo "OK! oom was not invoked"
+    EXIT=0
+fi
